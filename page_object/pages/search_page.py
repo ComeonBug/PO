cdcddccb21a4d9aa229baef7daabba2eeacef5e2
bluebluepupu <309351223@qq.com>
2020-10-18 20:55:14 +0800

    封装client

--- page_object/pages/search_page.py
+++ page_object/pages/search_page.py
@@ -2,7 +2,7 @@ import time
 
 from selenium.webdriver.common.by import By
 
-from page_object.drivers.android_client import AndroidClient
+from page_object.drivers.client import AndroidClient
 from page_object.pages.base_page import BasePage
 
 
